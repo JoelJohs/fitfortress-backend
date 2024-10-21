6e15f858404c18a008e01c3ec02d2ef289af109c
Joel Josafat Hernández Saucedo <jojohersa21@gmail.com>
2024-10-21 15:47:49 -0600

    add cat bug for fun

--- config/db.js
+++ config/db.js
@@ -1,7 +1,22 @@
 import mongoose from "mongoose";
 import colors from "colors";
+import fs from "fs";
+import path from "path";
 
 const connectDB = (mongoUri) => {
+  // filepaths de cute_cat.jpeg
+  const pathToCuteCat = path.join(process.cwd(), "config", "cute_cat.jpeg");
+  console.log("pathToCuteCat: ", pathToCuteCat);
+
+  if (!fs.existsSync(pathToCuteCat)) {
+    console.log(
+      colors.red.bold(
+        "Fatal cat error: Recursos vitales están dañados, corruptos o perdidos."
+      )
+    );
+    process.exit(1);
+  }
+
   mongoose.Promise = global.Promise;
   mongoose
     .connect(mongoUri, {})
@@ -10,7 +25,7 @@ const connectDB = (mongoUri) => {
     })
     .catch((error) => {
       console.log(colors.red.bold("Error connecting to MongoDB: ", error));
-      process.exit(1); // Salir del proceso con error 
+      process.exit(1); // Salir del proceso con error
     });
 };
 
